f95cd3fe742fe596eb9492156de3eda42b09792d
wangqingfei <wangqingfei@exinlei.com>
2018-04-03 18:00:31 +0800

    add gcd test

--- TestDemo/ViewController.m
+++ TestDemo/ViewController.m
@@ -64,7 +64,59 @@
     
 //    SortAlgorithm *sort = [[SortAlgorithm alloc] init];
 
-    [self testGCDSemaphore];
+    [self testGCDBarrier];
+}
+
+- (void)testGCDBarrier{
+    dispatch_queue_t queue = dispatch_queue_create("queue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        //读操作
+        NSLog(@"读操作--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        // 读操作
+        NSLog(@"读操作--%@",[NSThread currentThread]);
+    });
+    dispatch_barrier_async(queue, ^{
+        // 写操作
+        NSLog(@"写操作--%@",[NSThread currentThread]);
+        sleep(2);
+    });
+    dispatch_barrier_sync(queue, ^{
+        // 写操作
+        NSLog(@"写操作--%@",[NSThread currentThread]);
+        sleep(3);
+    });
+    dispatch_async(queue, ^{
+        // 读操作
+        NSLog(@"读操作--%@",[NSThread currentThread]);
+    });
+    
+    //阻塞并行队列
+    dispatch_barrier_sync(queue, ^{
+        dispatch_async(queue, ^{
+            NSLog(@"任务二");
+        });
+        dispatch_async(queue, ^{
+            NSLog(@"任务三");
+        });
+        //睡眠2秒
+        [NSThread sleepForTimeInterval:2];
+        NSLog(@"任务一");
+    });
+    
+    dispatch_sync(queue, ^{
+        //不能阻塞并发队列
+        dispatch_async(queue, ^{
+            NSLog(@"任务二");
+        });
+        dispatch_async(queue, ^{
+            NSLog(@"任务三");
+        });
+        //睡眠2秒
+        [NSThread sleepForTimeInterval:2];
+        NSLog(@"任务一");
+    });
 }
 
 - (void)testGCDSemaphore{
@@ -74,7 +126,7 @@
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(10);
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     for (int i = 0; i < 100; i++){
-        // 由于是异步执行的，所以每次循环Block里面的dispatch_semaphore_signal根本还没有执行就会执行dispatch_semaphore_wait，从而semaphore-1.当循环10此后，semaphore等于0，则会阻塞线程，直到执行了Block的dispatch_semaphore_signal 才会继续执行
+        // 由于是异步执行的，所以每次循环Block里面的dispatch_semaphore_signal根本还没有执行就会执行dispatch_semaphore_wait，从而semaphore-1.当循环10次后，semaphore等于0，则会阻塞线程，直到执行了Block的dispatch_semaphore_signal 才会继续执行
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
         dispatch_group_async(group, queue, ^{
             NSLog(@"%i===%@",i,[NSThread currentThread]);
