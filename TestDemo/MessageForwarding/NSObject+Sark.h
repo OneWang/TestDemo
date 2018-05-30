797d60123008f09a8c8961004cb48688fe83357c
wangqingfei <wangqingfei@exinlei.com>
2018-05-30 22:45:16 +0800

    add

---
+++ TestDemo/MessageForwarding/NSObject+Sark.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Sark.h
+//  TestDemo
+//
+//  Created by Jack on 2018/5/30.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Sark)
++ (void)foo;
+@end
---
+++ TestDemo/MessageForwarding/RunTimeViewController.m
@@ -0,0 +1,26 @@
+//
+//  RunTimeViewController.m
+//  TestDemo
+//
+//  Created by Jack on 2018/5/30.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+#import "RunTimeViewController.h"
+#import "NSObject+Sark.h"
+
+@interface RunTimeViewController ()
+
+@end
+
+@implementation RunTimeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [NSObject foo];
+    [[NSObject new] performSelector:@selector(foo)];
+}
+
+
+@end
